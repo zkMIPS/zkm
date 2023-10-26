b953e3b30f8c92a330156712d34647c244b76189
eigmax <stephen@ieigen.com>
2023-10-26 08:56:45 +0800

    feat: impl register and cpu

--- src/generation/mod.rs
+++ src/generation/mod.rs
@@ -21,8 +21,9 @@ use crate::generation::outputs::{get_outputs, GenerationOutputs};
 use crate::generation::state::GenerationState;
 use crate::witness::transition::transition;
 
+/// FIXME: useful?
 #[derive(Clone, Debug, Deserialize, Serialize, Default)]
-pub struct MipsTrace {
+pub(crate) struct MipsTrace {
     pub cycle: u32,
     pub pc: u32,
     pub next_pc: u32,
