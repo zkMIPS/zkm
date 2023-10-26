ed2f9d51c8625ce00c5200f8d4f79770c68d18f5
eigmax <stephen@ieigen.com>
2023-10-26 20:48:15 +0800

    chore: polish code

--- src/witness/state.rs
+++ src/witness/state.rs
@@ -4,6 +4,10 @@ const KERNEL_CONTEXT: usize = 0;
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct RegistersState {
+    pub gprs: [usize; 32],
+    pub lo: usize,
+    pub hi: usize,
+    pub heap: usize,
     pub program_counter: usize,
     pub is_kernel: bool,
     pub context: usize,
@@ -23,7 +27,11 @@ impl Default for RegistersState {
     fn default() -> Self {
         Self {
             // FIXME: fill in pc
-            program_counter: 0,
+            gprs: Default::default(),
+            lo: 0,
+            hi: 0,
+            heap: 0,
+            program_counter: KERNEL.global_labels["entry"],
             is_kernel: true,
             context: 0,
         }
