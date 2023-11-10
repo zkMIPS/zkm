638f6beedf03c9c967c3695659e4c7314a2ce446
brucelynnzkm <140376572+brucelynnzkm@users.noreply.github.com>
2023-11-10 17:29:50 +0800

    add syscall

--- src/cpu/cpu_stark.rs
+++ src/cpu/cpu_stark.rs
@@ -15,9 +15,7 @@ use crate::all_stark::Table;
 use crate::constraint_consumer::{ConstraintConsumer, RecursiveConstraintConsumer};
 use crate::cpu::columns::{COL_MAP, NUM_CPU_COLUMNS};
 use crate::cpu::membus::NUM_GP_CHANNELS;
-use crate::cpu::{
-    bootstrap_kernel, control_flow, decode, jumps, membus, memio, mov, pc, shift, simple_logic,
-};
+use crate::cpu::{bootstrap_kernel, control_flow, decode, jumps, membus, memio, mov, pc, shift, simple_logic, syscall};
 use crate::cross_table_lookup::{Column, TableWithColumns};
 use crate::evaluation_frame::{StarkEvaluationFrame, StarkFrame};
 use crate::memory::segments::Segment;
@@ -238,6 +236,8 @@ impl<F: RichField + Extendable<D>, const D: usize> Stark<F, D> for CpuStark<F, D
         syscalls_exceptions::eval_packed(local_values, next_values, yield_constr);
         */
         mov::eval_packed(local_values, yield_constr);
+        syscall::eval_packed(local_values, yield_constr);
+
     }
 
     fn eval_ext_circuit(
@@ -287,6 +287,7 @@ impl<F: RichField + Extendable<D>, const D: usize> Stark<F, D> for CpuStark<F, D
         syscalls_exceptions::eval_ext_circuit(builder, local_values, next_values, yield_constr);
         */
         mov::eval_ext_circuit(builder, local_values, yield_constr);
+        syscall::eval_ext_circuit(builder, local_values, yield_constr);
     }
 
     fn constraint_degree(&self) -> usize {
