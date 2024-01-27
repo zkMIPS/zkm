75f1be1154d85f051a7fa12fd345059547d68373
eigmax <stephen@ieigen.com>
2024-01-27 22:31:05 +0800

    fix: image id

--- src/cpu/bootstrap_kernel.rs
+++ src/cpu/bootstrap_kernel.rs
@@ -55,7 +55,7 @@ pub(crate) fn generate_bootstrap_kernel<F: Field>(state: &mut GenerationState<F>
     // The Keccak sponge CTL uses memory value columns for its inputs and outputs.
     final_cpu_row.mem_channels[0].value[0] = F::ZERO; // context
     final_cpu_row.mem_channels[1].value[0] = F::from_canonical_usize(Segment::Code as usize); // segment
-    let final_idx = image_addr_value_byte.len()/136 * 136;
+    let final_idx = image_addr_value_byte.len()/136 * 34;
     final_cpu_row.mem_channels[2].value[0] = F::from_canonical_usize(image_addr[final_idx].virt); // FIXME: virt should start from 0 or 65536?
     final_cpu_row.mem_channels[3].value[0] = F::from_canonical_usize(image_addr_value_byte.len()); // len
 
