401a140616f347af476c54891a8fc4f0a3d029d6
weilzkm <140377101+weilzkm@users.noreply.github.com>
2024-04-13 11:44:47 +0800

    feat: reduce the degree of syscall to 3 (#115)
    * feat: reduce the degree of syscall to 3
    
    * add missed filter in memio
    
    * increase size of arthnmetic table
    
    * update degree range
    
    * restore other config
    
    * update F:ONE

--- src/cpu/columns/general.rs
+++ src/cpu/columns/general.rs
@@ -85,6 +85,7 @@ impl<T: Copy> BorrowMut<[T; NUM_SHARED_COLUMNS]> for CpuGeneralColumnsView<T> {
 
 #[derive(Copy, Clone)]
 pub(crate) struct CpuSyscallView<T: Copy> {
+    pub(crate) cond: [T; 10],
     pub(crate) sysnum: [T; 11],
     pub(crate) a0: [T; 3],
     pub(crate) a1: T,
