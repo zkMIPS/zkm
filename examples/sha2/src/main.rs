09fd5dae9e72308ab53791087712176f8a5a128f
Angell Li <angell.l@zkm.io>
2024-07-03 12:59:12 +0800

    add port header and update sha2 bench test

--- examples/sha2/src/main.rs
+++ examples/sha2/src/main.rs
@@ -5,14 +5,14 @@ use sha2::{Digest, Sha256};
 extern crate alloc;
 use alloc::vec::Vec;
 
-zkm_zkvm::entrypoint!(main);
+zkm_runtime::entrypoint!(main);
 
 pub fn main() {
-    let input: Vec<u8> = zkm_zkvm::io::read();
+    let input: Vec<u8> = zkm_runtime::io::read();
 
     let mut hasher = Sha256::new();
     hasher.update(input);
     let result = hasher.finalize();
 
-    zkm_zkvm::io::commit::<[u8; 32]>(&result.into());
+    zkm_runtime::io::commit::<[u8; 32]>(&result.into());
 }
