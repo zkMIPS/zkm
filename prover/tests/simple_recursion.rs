42dac2227cee2f78606ff8206b9aa76a24ff3712
zkmod <148928866+zkmod@users.noreply.github.com>
2024-07-16 17:42:07 +0800

    fix: public inputs  (#148)
    * update dependency
    
    * Update dependency
    
    * fix clippy

--- prover/tests/simple_recursion.rs
+++ prover/tests/simple_recursion.rs
@@ -1,22 +1,10 @@
 #![allow(clippy::upper_case_acronyms)]
-use std::fs::File;
-use std::time::Duration;
-
 use plonky2::field::goldilocks_field::GoldilocksField;
 use plonky2::field::types::Field;
 use plonky2::iop::witness::{PartialWitness, WitnessWrite};
 use plonky2::plonk::circuit_builder::CircuitBuilder;
 use plonky2::plonk::circuit_data::CircuitConfig;
 use plonky2::plonk::config::PoseidonGoldilocksConfig;
-use std::io::BufReader;
-use zkm_emulator::utils::{load_elf_with_patch, split_prog_into_segs};
-use zkm_prover::all_stark::AllStark;
-use zkm_prover::config::StarkConfig;
-use zkm_prover::cpu::kernel::assembler::segment_kernel;
-use zkm_prover::fixed_recursive_verifier::AllRecursiveCircuits;
-use zkm_prover::proof::PublicValues;
-
-use plonky2::util::timing::TimingTree;
 
 type F = GoldilocksField;
 const D: usize = 2;
@@ -84,116 +72,15 @@ fn test_mips_with_aggreg_fibo() -> anyhow::Result<()> {
     let mut circuit2 = builder.build();
     circuit2.set_data(data);
 
-    let wrapped_circuit = WrappedCircuit::<InnerParameters, OuterParameters, D>::build(circuit2);
+    let wrapped_circuit = WrappedCircuit::<InnerParameters, OuterParameters, D>::build(
+        circuit2,
+        Some((vec![], vec![8, 8, 8])), // bit length of public inputs
+    );
 
     let build_path = "../verifier/data".to_string();
     let path = format!("{}/test_circuit/", build_path);
 
     let wrapped_proof = wrapped_circuit.prove(&proof).unwrap();
     wrapped_proof.save(path).unwrap();
-    Ok(())
-}
-
-#[test]
-#[ignore = "Two slow"]
-fn test_mips_with_aggreg() -> anyhow::Result<()> {
-    use plonky2x::backend::circuit::Groth16WrapperParameters;
-    use plonky2x::backend::wrapper::wrap::WrappedCircuit;
-    use plonky2x::frontend::builder::CircuitBuilder as WrapperBuilder;
-    use plonky2x::prelude::DefaultParameters;
-
-    let seg_output = "/tmp/mips_output";
-    let elf_path = "../emualtor/test-vectors/hello";
-    let seg_size = 65536;
-
-    let state = load_elf_with_patch(elf_path, vec![]);
-    let _ = split_prog_into_segs(state, seg_output, "", seg_size);
-
-    type InnerParameters = DefaultParameters;
-    type OuterParameters = Groth16WrapperParameters;
-
-    env_logger::try_init().unwrap_or_default();
-
-    let all_stark = AllStark::<F, D>::default();
-    let config = StarkConfig::standard_fast_config();
-    // Preprocess all circuits.
-    let all_circuits = AllRecursiveCircuits::<F, C, D>::new(
-        &all_stark,
-        &[10..20, 10..22, 8..19, 8..17, 12..20, 14..23],
-        &config,
-    );
-
-    let seg_file = format!("{}/0", seg_output);
-    let seg_reader = BufReader::new(File::open(seg_file)?);
-    let input_first = segment_kernel("", "", "", seg_reader, seg_size);
-    let mut timing = TimingTree::new("prove root first", log::Level::Info);
-    let (root_proof_first, first_public_values) =
-        all_circuits.prove_root(&all_stark, &input_first, &config, &mut timing)?;
-
-    timing.filter(Duration::from_millis(100)).print();
-    all_circuits.verify_root(root_proof_first.clone())?;
-
-    let seg_file = format!("{}/1", seg_output);
-    let seg_reader = BufReader::new(File::open(seg_file)?);
-    let input = segment_kernel("", "", "", seg_reader, seg_size);
-    let mut timing = TimingTree::new("prove root second", log::Level::Info);
-    let (root_proof, public_values) =
-        all_circuits.prove_root(&all_stark, &input, &config, &mut timing)?;
-    timing.filter(Duration::from_millis(100)).print();
-
-    all_circuits.verify_root(root_proof.clone())?;
-    log::trace!("public_inputs before {:?}", first_public_values);
-    log::trace!("public_inputs {:?}", public_values);
-
-    // Update public values for the aggregation.
-    let agg_public_values = PublicValues {
-        roots_before: first_public_values.roots_before,
-        roots_after: public_values.roots_after,
-        userdata: vec![],
-    };
-
-    // We can duplicate the proofs here because the state hasn't mutated.
-    log::info!("prove aggregation");
-    let (agg_proof, updated_agg_public_values) = all_circuits.prove_aggregation(
-        false,
-        &root_proof_first,
-        false,
-        &root_proof,
-        agg_public_values,
-    )?;
-    log::info!("verify aggregation");
-    all_circuits.verify_aggregation(&agg_proof)?;
-    log::info!("prove aggregation successfully");
-    let (block_proof, _block_public_values) =
-        all_circuits.prove_block(None, &agg_proof, updated_agg_public_values)?;
-
-    log::info!(
-        "proof size: {:?}",
-        serde_json::to_string(&block_proof.proof).unwrap().len()
-    );
-    let _ = all_circuits.verify_block(&block_proof);
-    println!(
-        "all_circuits.block.circuit.common.num_public_inputs {:?}",
-        all_circuits.block.circuit.common.num_public_inputs
-    );
-    println!("block_proof.public_inputs {:?}", block_proof.public_inputs);
-
-    let build_path = "../verifier/data".to_string();
-    let path = format!("{}/test_circuit/", build_path);
-    let builder = WrapperBuilder::<DefaultParameters, 2>::new();
-    let mut circuit = builder.build();
-    circuit.set_data(all_circuits.block.circuit);
-    let wrapped_circuit = WrappedCircuit::<InnerParameters, OuterParameters, D>::build(circuit);
-    println!("build finish");
-
-    // let (proof_inputs,_) = block_proof.public_inputs.split_at(2);
-    // let proof_conv =  ProofWithPublicInputs {
-    //     proof:block_proof.proof,
-    //     public_inputs: Vec::from(proof_inputs),
-    // };
-
-    let wrapped_proof = wrapped_circuit.prove(&block_proof).unwrap();
-    wrapped_proof.save(path).unwrap();
-
     Ok(())
 }
