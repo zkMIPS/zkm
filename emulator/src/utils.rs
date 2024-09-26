935a6896c9bf0773f369ade53af1964a6c5f7b99
Angell Li <angell.l@zkm.io>
2024-09-26 11:12:47 +0800

    update default segment size to 65536

--- emulator/src/utils.rs
+++ emulator/src/utils.rs
@@ -3,7 +3,7 @@ use elf::{endian::AnyEndian, ElfBytes};
 use std::fs;
 use std::fs::File;
 
-pub const SEGMENT_STEPS: usize = 1024;
+pub const SEGMENT_STEPS: usize = 65536;
 
 /// From the minigeth's rule, the `block` starts with `0_`
 pub fn get_block_path(basedir: &str, block: &str, file: &str) -> String {
