f1ba81b81f78fe16de94fd29aa69c71ac970e6f5
Angell Li <angell.l@zkm.io>
2024-10-22 22:00:51 +0800

    fix input for common program

--- prover/examples/zkmips.rs
+++ prover/examples/zkmips.rs
@@ -35,13 +35,13 @@ fn split_segments() {
     let seg_path = env::var("SEG_OUTPUT").expect("Segment output path is missing");
     let seg_size = env::var("SEG_SIZE").unwrap_or(format!("{SEGMENT_STEPS}"));
     let seg_size = seg_size.parse::<_>().unwrap_or(SEGMENT_STEPS);
-    let args = env::var("ARGS").unwrap_or("data-to-hash".to_string());
+    let args = env::var("ARGS").unwrap_or("".to_string());
     // assume the first arg is the hash output(which is a public input), and the others are the input.
     let args: Vec<&str> = args.split_whitespace().collect();
     let mut state = load_elf_with_patch(&elf_path, vec![]);
 
     if args.len() > 0 {
-        let public_input: Vec<u8> = hex::decode(args[0]).unwrap();
+        let public_input: Vec<u8> = args[0].as_bytes().to_vec();
         state.add_input_stream(&public_input);
     }
 
