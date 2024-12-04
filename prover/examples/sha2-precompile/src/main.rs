ea8840ca09a6b3badd1c48c0975f97a3d5b1d23b
weilzkm <140377101+weilzkm@users.noreply.github.com>
2024-12-04 08:00:34 +0800

    fix: generate image id for precompile elf dynamicaly (#187)
    (cherry picked from commit 08629aecb2e87aa752755c91193e5860fe6c8ee7)

--- prover/examples/sha2-precompile/src/main.rs
+++ prover/examples/sha2-precompile/src/main.rs
@@ -6,14 +6,13 @@ extern crate alloc;
 use alloc::vec::Vec;
 
 zkm_runtime::entrypoint!(main);
-const SHA_ELF:[u8; 32] = [83, 70, 149, 120, 71, 122, 247, 101, 174, 227, 186, 199, 6, 32, 152, 39, 176, 153, 148, 65, 154, 248, 140, 95, 163, 122, 249, 151, 112, 84, 68, 192];
 
 pub fn main() {
     let public_input: Vec<u8> = zkm_runtime::io::read();
     let input: Vec<u8> = zkm_runtime::io::read();
+    let elf_id: Vec<u8> = zkm_runtime::io::read();
 
-
-    zkm_runtime::io::verify(SHA_ELF.to_vec(), &input);
+    zkm_runtime::io::verify(elf_id, &input);
     let mut hasher = Sha256::new();
     hasher.update(input);
     let result = hasher.finalize();
