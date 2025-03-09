974d6f5e21e6b382e8acc47a0a18779d94cae7ff
Angell Li <angell.l@zkm.io>
2025-03-09 10:50:02 +0800

    optimize memcpy by hand

--- runtime/entrypoint/src/memcpy.s
+++ runtime/entrypoint/src/memcpy.s
@@ -216,7 +216,7 @@ memcpy:                                # @memcpy
 	.set	noreorder
 	.set	nomacro
 	.set	noat
-# %bb.0:
+# %bb.0: #23
 	addiu	$sp, $sp, -8
 	sw	$ra, 4($sp)                     # 4-byte Folded Spill
 	sw	$fp, 0($sp)                     # 4-byte Folded Spill
@@ -224,14 +224,14 @@ memcpy:                                # @memcpy
 	andi	$1, $5, 3
 	beqz	$1, $BB0_15
 	nop
-# %bb.1:
+# %bb.1:  #23
 	beqz	$6, $BB0_6
 	nop
-# %bb.2:
+# %bb.2:  #23
 	addiu	$3, $5, 1
 	addiu	$7, $zero, 1
 	move	$2, $4
-$BB0_3:                                 # =>This Inner Loop Header: Depth=1
+$BB0_3:   #23                              # =>This Inner Loop Header: Depth=1
 	lbu	$1, 0($5)
 	move	$8, $6
 	addiu	$6, $6, -1
@@ -246,81 +246,48 @@ $BB0_3:                                 # =>This Inner Loop Header: Depth=1
 # %bb.5:
 	j	$BB0_7
 	nop
-$BB0_6:
+$BB0_6: #23
 	move	$2, $4
-$BB0_7:
+$BB0_7: #25
 	andi	$7, $2, 3
 	beqz	$7, $BB0_16
 	nop
-$BB0_8:
-	sltiu	$1, $6, 32
-	bnez	$1, $BB0_31
-	nop
-# %bb.9:
-	lw	$3, 0($5)
-	addiu	$1, $zero, 3
-	beq	$7, $1, $BB0_28
-	nop
-# %bb.10:
-	addiu	$1, $zero, 2
-	bne	$7, $1, $BB0_36
-	nop
-# %bb.11:
-	srl	$7, $3, 16
-	srl	$1, $3, 24
-	addiu	$5, $5, 16
-	addiu	$6, $6, -2
-	sb	$7, 1($2)
-	addiu	$7, $2, 2
-	sb	$1, 0($2)
-$BB0_12:                                # =>This Inner Loop Header: Depth=1
-	lw	$2, -12($5)
-	srl	$1, $3, 16
-	addiu	$8, $5, 16
-	addiu	$6, $6, -16
-	sll	$3, $2, 16
-	or	$1, $3, $1
-	lw	$3, -8($5)
-	sw	$1, 0($7)
-	srl	$1, $2, 16
-	addiu	$2, $7, 16
-	sll	$9, $3, 16
-	or	$1, $9, $1
-	sltiu	$9, $6, 18
-	sw	$1, 4($7)
-	srl	$1, $3, 16
-	lw	$3, -4($5)
-	sll	$10, $3, 16
-	or	$1, $10, $1
-	sw	$1, 8($7)
-	srl	$1, $3, 16
-	lw	$3, 0($5)
-	sll	$5, $3, 16
-	or	$1, $5, $1
-	move	$5, $8
-	sw	$1, 12($7)
-	beqz	$9, $BB0_12
-	move	$7, $2
-# %bb.13:
+$BB0_8: #50
 	sltiu	$1, $6, 16
-	beqz	$1, $BB0_39
-	addiu	$5, $8, -14
+	bnez	$1, $BB0_32
+	nop
+$BB0_12: #51~106
+	lw	$3, 0($5)
+	swl $3, 0($2)
+	swr $3, 3($2)
+	lw	$3, 4($5)
+	swl $3, 4($2)
+	swr $3, 7($2)
+	lw	$3, 8($5)
+	swl $3, 8($2)
+	swr $3, 11($2)
+	lw	$3, 12($5)
+	swl $3, 12($2)
+	swr $3, 15($2)
+	addiu	$5, $5, 16
+	addiu	$6, $6, -16
+	sltiu 	$9, $6, 16
+	beqz	$9, $BB0_12
+	addiu   $2, $2, 16
 # %bb.14:
 	j	$BB0_32
 	nop
-$BB0_15:
+$BB0_15:  #23
 	move	$2, $4
 	andi	$7, $2, 3
 	bnez	$7, $BB0_8
 	nop
-$BB0_16:
+$BB0_16: #26
 	sltiu	$1, $6, 16
 	bnez	$1, $BB0_23
 	nop
-$BB0_17:                                # =>This Inner Loop Header: Depth=1
+$BB0_17: #27~30                              # =>This Inner Loop Header: Depth=1
 	lw	$1, 0($5)
-	addiu	$3, $2, 16
-	addiu	$7, $5, 16
 	addiu	$6, $6, -16
 	sw	$1, 0($2)
 	lw	$1, 4($5)
@@ -328,249 +295,110 @@ $BB0_17:                                # =>This Inner Loop Header: Depth=1
 	lw	$1, 8($5)
 	sw	$1, 8($2)
 	lw	$1, 12($5)
-	move	$5, $7
+	addiu	$5, $5, 16
 	sw	$1, 12($2)
 	sltiu	$1, $6, 16
 	beqz	$1, $BB0_17
-	move	$2, $3
-# %bb.18:
+	addiu	$2, $2, 16
+# %bb.18: #32
 	sltiu	$1, $6, 8
 	beqz	$1, $BB0_24
 	nop
-$BB0_19:
+$BB0_19: #37
 	andi	$1, $6, 4
 	bnez	$1, $BB0_25
 	nop
-$BB0_20:
+$BB0_20: #41
 	andi	$1, $6, 2
 	bnez	$1, $BB0_26
 	nop
-$BB0_21:
+$BB0_21: #44
 	andi	$1, $6, 1
 	beqz	$1, $BB0_45
 	nop
-# %bb.22:
+# %bb.22: #47
 	j	$BB0_44
 	nop
-$BB0_23:
-	move	$3, $2
+$BB0_23: #32
 	sltiu	$1, $6, 8
 	bnez	$1, $BB0_19
-	move	$7, $5
-$BB0_24:
-	lw	$1, 0($7)
-	sw	$1, 0($3)
-	lw	$1, 4($7)
-	addiu	$7, $7, 8
-	sw	$1, 4($3)
+	nop
+$BB0_24: #33-34
+	lw	$1, 0($5)
+	sw	$1, 0($2)
+	lw	$1, 4($5)
+	addiu	$5, $5, 8
+	sw	$1, 4($2)
 	andi	$1, $6, 4
 	beqz	$1, $BB0_20
-	addiu	$3, $3, 8
-$BB0_25:
-	lw	$1, 0($7)
-	addiu	$7, $7, 4
-	sw	$1, 0($3)
+	addiu	$2, $2, 8
+$BB0_25: #38-39
+	lw	$1, 0($5)
+	addiu	$5, $5, 4
+	sw	$1, 0($2)
 	andi	$1, $6, 2
 	beqz	$1, $BB0_21
-	addiu	$3, $3, 4
-$BB0_26:
-	lbu	$1, 0($7)
-	sb	$1, 0($3)
-	lbu	$1, 1($7)
-	addiu	$7, $7, 2
-	sb	$1, 1($3)
+	addiu	$2, $2, 4
+$BB0_26:  #42
+	lhu	$1, 0($5)
+	addiu	$5, $5, 2
+	sh	$1, 0($2)
 	andi	$1, $6, 1
 	beqz	$1, $BB0_45
-	addiu	$3, $3, 2
+	addiu	$2, $2, 2
 # %bb.27:
 	j	$BB0_44
 	nop
-$BB0_28:
-	srl	$1, $3, 24
-	addiu	$5, $5, 16
-	addiu	$6, $6, -1
-	addiu	$7, $2, 1
-	sb	$1, 0($2)
-$BB0_29:                                # =>This Inner Loop Header: Depth=1
-	lw	$2, -12($5)
-	srl	$1, $3, 8
-	addiu	$8, $5, 16
-	addiu	$6, $6, -16
-	sll	$3, $2, 24
-	or	$1, $3, $1
-	lw	$3, -8($5)
-	sw	$1, 0($7)
-	srl	$1, $2, 8
-	addiu	$2, $7, 16
-	sll	$9, $3, 24
-	or	$1, $9, $1
-	sltiu	$9, $6, 19
-	sw	$1, 4($7)
-	srl	$1, $3, 8
-	lw	$3, -4($5)
-	sll	$10, $3, 24
-	or	$1, $10, $1
-	sw	$1, 8($7)
-	srl	$1, $3, 8
-	lw	$3, 0($5)
-	sll	$5, $3, 24
-	or	$1, $5, $1
-	move	$5, $8
-	sw	$1, 12($7)
-	beqz	$9, $BB0_29
-	move	$7, $2
-# %bb.30:
-	addiu	$5, $8, -15
-$BB0_31:
-	sltiu	$1, $6, 16
-	beqz	$1, $BB0_39
-	nop
 $BB0_32:
-	move	$3, $2
 	andi	$1, $6, 8
-	beqz	$1, $BB0_40
-	move	$7, $5
-$BB0_33:
-	lbu	$1, 0($7)
-	addiu	$2, $3, 8
-	sb	$1, 0($3)
-	lbu	$1, 1($7)
-	sb	$1, 1($3)
-	lbu	$1, 2($7)
-	sb	$1, 2($3)
-	lbu	$1, 3($7)
-	sb	$1, 3($3)
-	lbu	$1, 4($7)
-	sb	$1, 4($3)
-	lbu	$1, 5($7)
-	sb	$1, 5($3)
-	lbu	$1, 6($7)
-	sb	$1, 6($3)
-	lbu	$1, 7($7)
-	sb	$1, 7($3)
+	beqz	$1, $BB0_40     #107
+	nop
+$BB0_33:   #108~109
+	lw	$1, 0($5)
+	swl	$1, 0($2)
+	swr $1, 3($2)
+	lw	$1, 4($5)
+	swl	$1, 4($2)
+	swr $1, 7($2)
+	addiu	$2, $2, 8
 	andi	$1, $6, 4
-	beqz	$1, $BB0_41
-	addiu	$5, $7, 8
-$BB0_34:
-	lbu	$1, 0($5)
-	addiu	$3, $2, 4
-	sb	$1, 0($2)
-	lbu	$1, 1($5)
-	sb	$1, 1($2)
-	lbu	$1, 2($5)
-	sb	$1, 2($2)
-	lbu	$1, 3($5)
-	sb	$1, 3($2)
+	beqz	$1, $BB0_41  #111
+	addiu	$5, $5, 8
+$BB0_34:  #112
+	lw	$1, 0($5)
+	swl	$1, 0($2)
+	swr $1, 3($2)
+	addiu	$2, $2, 4
 	andi	$1, $6, 2
-	beqz	$1, $BB0_43
-	addiu	$7, $5, 4
-# %bb.35:
+	beqz	$1, $BB0_43 #117
+	addiu	$5, $5, 4
+# %bb.35:  #120
 	j	$BB0_42
 	nop
-$BB0_36:
-	srl	$8, $3, 8
-	srl	$7, $3, 16
-	srl	$1, $3, 24
-	addiu	$5, $5, 16
-	addiu	$6, $6, -3
-	sb	$8, 2($2)
-	sb	$7, 1($2)
-	addiu	$7, $2, 3
-	sb	$1, 0($2)
-$BB0_37:                                # =>This Inner Loop Header: Depth=1
-	lw	$2, -12($5)
-	srl	$1, $3, 24
-	addiu	$8, $5, 16
-	addiu	$6, $6, -16
-	sll	$3, $2, 8
-	or	$1, $3, $1
-	lw	$3, -8($5)
-	sw	$1, 0($7)
-	srl	$1, $2, 24
-	addiu	$2, $7, 16
-	sll	$9, $3, 8
-	or	$1, $9, $1
-	sltiu	$9, $6, 17
-	sw	$1, 4($7)
-	srl	$1, $3, 24
-	lw	$3, -4($5)
-	sll	$10, $3, 8
-	or	$1, $10, $1
-	sw	$1, 8($7)
-	srl	$1, $3, 24
-	lw	$3, 0($5)
-	sll	$5, $3, 8
-	or	$1, $5, $1
-	move	$5, $8
-	sw	$1, 12($7)
-	beqz	$9, $BB0_37
-	move	$7, $2
-# %bb.38:
-	sltiu	$1, $6, 16
-	bnez	$1, $BB0_32
-	addiu	$5, $8, -13
-$BB0_39:
-	lbu	$1, 0($5)
-	addiu	$3, $2, 16
-	sb	$1, 0($2)
-	lbu	$1, 1($5)
-	sb	$1, 1($2)
-	lbu	$1, 2($5)
-	sb	$1, 2($2)
-	lbu	$1, 3($5)
-	sb	$1, 3($2)
-	lbu	$1, 4($5)
-	sb	$1, 4($2)
-	lbu	$1, 5($5)
-	sb	$1, 5($2)
-	lbu	$1, 6($5)
-	sb	$1, 6($2)
-	lbu	$1, 7($5)
-	sb	$1, 7($2)
-	lbu	$1, 8($5)
-	sb	$1, 8($2)
-	lbu	$1, 9($5)
-	sb	$1, 9($2)
-	lbu	$1, 10($5)
-	sb	$1, 10($2)
-	lbu	$1, 11($5)
-	sb	$1, 11($2)
-	lbu	$1, 12($5)
-	sb	$1, 12($2)
-	lbu	$1, 13($5)
-	sb	$1, 13($2)
-	lbu	$1, 14($5)
-	sb	$1, 14($2)
-	lbu	$1, 15($5)
-	sb	$1, 15($2)
-	andi	$1, $6, 8
-	bnez	$1, $BB0_33
-	addiu	$7, $5, 16
+
 $BB0_40:
-	move	$2, $3
 	andi	$1, $6, 4
 	bnez	$1, $BB0_34
-	move	$5, $7
-$BB0_41:
-	move	$3, $2
+	nop
+$BB0_41: #114
 	andi	$1, $6, 2
 	beqz	$1, $BB0_43
-	move	$7, $5
+	nop
 $BB0_42:
-	lbu	$1, 0($7)
-	sb	$1, 0($3)
-	lbu	$1, 1($7)
-	addiu	$7, $7, 2
-	sb	$1, 1($3)
-	addiu	$3, $3, 2
-$BB0_43:
+	lbu	$1, 0($5)
+	sb	$1, 0($2)
+	lbu	$1, 1($5)
+	addiu	$5, $5, 2
+	sb	$1, 1($2)
+	addiu	$2, $2, 2
+$BB0_43: #117
 	andi	$1, $6, 1
 	beqz	$1, $BB0_45
 	nop
-$BB0_44:
-	lbu	$1, 0($7)
-	sb	$1, 0($3)
-$BB0_45:
+$BB0_44: #118
+	lbu	$1, 0($5)
+	sb	$1, 0($2)
+$BB0_45: #120
 	move	$2, $4
 	move	$sp, $fp
 	lw	$fp, 0($sp)                     # 4-byte Folded Reload
